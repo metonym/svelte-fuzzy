a799b67be581f46f2dc1678f2041b2e84016068c
Eric Liu <ericyl.us@gmail.com>
2020-02-05 07:54:29 -0800

    fix(format): patch format-fuse.js

--- src/Fuzzy.svelte
+++ src/Fuzzy.svelte
@@ -1,25 +1,25 @@
 <script>
-  import Fuse from './Fuse.ts';
-  import format from 'format-fuse.js';
   export let query = '';
   export let data = [];
   export let options = {};
 
+  import Fuse from './Fuse.ts';
+  import format from 'format-fuse.js';
+
   options.shouldSort = true;
   options.includeMatches = true;
 
-  let results = [];
   const fuse = new Fuse(data, options);
+
+  let result = [];
 
   $: if (data) {
     fuse.setCollection(data);
   }
 
   $: if (query || data) {
-    results = fuse.search(query);
+    result = format(fuse.search(query));
   }
-
-  $: result = format(results);
 </script>
 
 <slot {result} results={result} />
