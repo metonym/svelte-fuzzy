ee4bbe4c1621f5c38531399b74e15087dc76d27f
Eric Liu <ericyl.us@gmail.com>
2020-02-21 08:13:14 -0800

    feat: add default slot with formatted results

--- src/Fuzzy.svelte
+++ src/Fuzzy.svelte
@@ -1,4 +1,6 @@
 <script>
+  let className = 'svelte-fuzzy';
+  export { className as class };
   export let query = '';
   export let data = [];
   export let options = {};
@@ -12,6 +14,7 @@
   const fuse = new Fuse(data, options);
 
   let result = [];
+  let formatted = [];
 
   $: if (data) {
     fuse.setCollection(data);
@@ -19,7 +22,36 @@
 
   $: if (query || data) {
     result = format(fuse.search(query));
+    formatted = result.map(item => {
+      return options.keys.map(key => {
+        return typeof item[key] === 'string'
+          ? [{ key, text: item[key], matches: false }]
+          : item[key].map(field => ({ ...field, key }));
+      });
+    });
   }
 </script>
 
-<slot {result} results={result} />
+<style>
+  .svelte-fuzzy-field {
+    display: block;
+  }
+</style>
+
+<slot results={result} {result} {formatted}>
+  {#each formatted as item}
+    <div class={className}>
+      {#each item as field}
+        <span class="svelte-fuzzy-field">
+          {#each field as { key, text, matches }}
+            <span class={`svelte-fuzzy-${key}`}>
+              {#if matches}
+                <mark>{text}</mark>
+              {:else}{text}{/if}
+            </span>
+          {/each}
+        </span>
+      {/each}
+    </div>
+  {/each}
+</slot>
--- src/client/App.svelte
+++ src/client/App.svelte
@@ -3,27 +3,22 @@
 
   let query = 'old';
   let data = [
-    { title: "Old Man's War" },
-    { title: 'The Lock Artist' },
-    { title: 'HTML5' }
+    {
+      title: "Old Man's War",
+      author: 'John'
+    },
+    {
+      title: 'The Lock Artist',
+      author: 'Steve'
+    },
+    {
+      title: 'Remy',
+      author: 'Remy'
+    }
   ];
+  let options = { keys: ['title', 'author'] };
 </script>
 
 <input bind:value={query} />
 
-<button
-  on:click={() => {
-    data = [...data, { title: 'Boulder' }];
-  }}>
-  Update data
-</button>
-
-<Fuzzy {query} {data} options={{ keys: ['title'] }} let:result>
-  {#each result as { title }}
-    {#each title as { text, matches }}
-      {#if matches}
-        <mark>{text}</mark>
-      {:else}{text}{/if}
-    {/each}
-  {/each}
-</Fuzzy>
+<Fuzzy {query} {data} {options} />
